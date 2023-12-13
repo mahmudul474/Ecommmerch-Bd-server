131d9f14d49d4ebc03385756a58c4a7bb2ff1599
Ataur Rahman <rahman.sharif39n@gmail.com>
2023-12-13 12:47:39 +0600

    forget password api added

--- src/pages/Forgotpassword.js
+++ src/pages/Forgotpassword.js
@@ -1,7 +1,38 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { toast } from "react-toastify";
 import CustomInput from "../components/CustomInput";
 
 const Forgotpassword = () => {
+
+  const [email, setEmail] = useState('')
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    console.log(email);
+    const loadingToast = toast.loading(`Please wait...`);
+    try {
+      const response = await axios.post('https://api.dreamfurniturebd.com/api/v1/auth/forget-password', {
+        email
+      })
+      console.log(response.data);
+      toast.update(loadingToast, {
+        render: response.data.data,
+        type: "success",
+        isLoading: false,
+        autoClose: 1000,
+      });
+    } catch (error) {
+      console.log(error?.response?.data?.message || error?.message);
+      toast.dismiss(loadingToast)
+      toast.error(`Something went wrong. ${error?.response?.data?.message || error?.message}`, {
+        toastId: "forget-request-error", // Use a unique toastId for error Toast
+        autoClose: 3000,
+        delay: 600
+      });
+    }
+  }
+
   return (
     <div className="py-5" style={{ background: "#ffd333", minHeight: "100vh" }}>
       <br />
@@ -14,15 +45,15 @@ const Forgotpassword = () => {
         <p className="text-center">
           Please Enter your register email to get reset password mail.
         </p>
-        <form action="">
-          <CustomInput type="password" label="Email Address" id="email" />
+        <form onSubmit={onSubmit}>
+          <CustomInput type="email" label="Email Address" id="email" onChng={(e) => setEmail(e.target.value)} />
 
           <button
-            className="border-0 px-3 py-2 text-white fw-bold w-100"
-            style={{ background: "#ffd333" }}
+            className="border-0 px-3 py-2 text-white fw-bold w-100 "
+            style={{ background: "#ffd333", marginTop: "10px" }}
             type="submit"
           >
-            Send Link
+            Continue
           </button>
         </form>
       </div>
